d863d4574a2452dd5e28bf573c5d22a111e688f8
Francisco Rodriguez <weareidt@gmail.com>
2022-07-10 21:09:30 -0500

    one last change

--- week03/hello.js
+++ week03/hello.js
@@ -1,6 +1,6 @@
 // this is the Hello World program for node.js
 
-var msg = 'This was a node.js program.'
+var msg = 'This is a node.js program.'
 
 console.log('Hello, World!');
 console.log('Programming is fun!');
