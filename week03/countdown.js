e3d91f7dae8d2b4de127ee104fe7ac616db756a6
itse1411 <70217924+itse1411@users.noreply.github.com>
2022-07-14 18:11:57 -0500

    initial code

--- week03/countdown.js
+++ week03/countdown.js
@@ -1,1 +1,11 @@
-// This is a blank file
+function countdown(seconds){
+  if(seconds >= 0) {
+    console.log(seconds);
+    seconds--;
+    setTimeout(function(){
+      countdown(seconds)
+    }, 1000);
+  }
+}
+
+countdown(10);
