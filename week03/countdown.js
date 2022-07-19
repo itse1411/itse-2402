b57ee6a098f55032b875e8be5ee659b0780d8221
Francisco Rodriguez <weareidt@gmail.com>
2022-07-19 16:31:55 -0500

    added week 07 content

--- week03/countdown.js
+++ week03/countdown.js
@@ -1,1 +1,12 @@
-// This is a blank file
+/*
+ kitchen countdown program
+ should countdown from predetermined number to 0
+ and look like sample at bottom
+*/
+
+countdown(3);
+
+// 3...
+// 2...
+// 1...
+// 0
